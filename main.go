1321c1ede246533a00f750b69ea1541e39856433
Sneh Khatri <khatris2014@gmail.com>
2025-12-07 14:05:38 +0530

    Add HTTP handlers for WebSocket and health check endpoints (#5)
    - Implemented a WebSocket handler at /ws that responds with a status OK.
    - Added a health check endpoint at /healthz that also responds with a status OK.
    - Included logging for server startup on port 8000.
    
    Co-authored-by: The-x-Theorist <snehkhatri@Snehs-MacBook-Air.local>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -10,4 +11,22 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+
+func main() {
+	NewGameServer()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	log.Println(("Arena dash server listening on port :8000"))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
